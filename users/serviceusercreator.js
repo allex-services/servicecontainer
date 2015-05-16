abf276e94cd1d37f174ad83670c31d4bfac68a13
Andrija Petrovic <andrija.hers@gmail.com>
2015-05-16 16:20:01 +0200

    Improving the generic service User

--- users/serviceusercreator.js
+++ users/serviceusercreator.js
@@ -26,14 +26,23 @@ function createServiceUser(execlib,ParentUser){
   ServiceUser.prototype._onSinkAcquired = function(defer,spawndescriptor,sink){
     var record = this._spawnDescriptorToRecord(spawndescriptor);
     record.closed = false;
+    var sinkinstancename = this._instanceNameFromRecord(record);
+    if(sinkinstancename){
+      this.__service.subservices.add(sinkinstancename,sink);
+    }
+    console.log('creating record from',record);
     this.__service.data.create(record).done(
       this._onServiceRecordCreated.bind(this,defer,sink),
       defer.reject.bind(defer)
     );
   };
   ServiceUser.prototype._onServiceRecordCreated = function(defer,sink,record){
+    console.log('record created',record,'from',this.__service.modulename);
     sink.consumeChannel('s',sink.extendTo(this.__service.data.stateStreamFilterForRecord(record)));
     defer.resolve(record);
+  };
+  ServiceUser.prototype._instanceNameFromRecord = function(record){
+    return record.instancename;
   };
   ServiceUser.prototype._spawnDescriptorToRecord = function(spawndescriptor){
     return spawndescriptor;
