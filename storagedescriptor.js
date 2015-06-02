f77dcb648a7fa359d5666c3aff07ae1e10a7ad32
Andrija Petrovic <andrija.hers@gmail.com>
2015-06-02 15:00:56 +0200

    ServiceContainerService does not define a Record any more - let ancestors define it

--- storagedescriptor.js
+++ storagedescriptor.js
@@ -1,18 +1,6 @@
 module.exports = {
   record:{
-    primaryKey: 'instancename',
-    fields:[{
-      name: 'instancename'
-    },{
-      name: 'modulename'
-    },{
-      name: 'propertyhash',
-      default: {}
-    },{
-      name: 'strategies',
-      default: {}
-    },{
-      name: 'closed',
-    }]
+    fields:[
+    ]
   }
 };
